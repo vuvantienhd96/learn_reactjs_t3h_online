ff5a4471dc9cfc84b1bb079512d4ae3c2a71140a
tienvv11 <congtuocxauxa000@gmail.com>
2021-10-29 19:09:38 +0700

    add file demo

---
+++ src/component/test.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class Test extends Component {
+    render() {
+        return (
+            <div>
+                xin chao 1
+            </div>
+        )
+    }
+}
+
+export const XinChao = class Hello extends Component {
+    render(){
+        return  <h2> xin chao 2</h2>
+      
+    }
+}
+
+export const XinChao3 = class Hello extends Component {
+    render(){
+        return  <h2> xin chao 3</h2>
+      
+    }
+}
+
+
